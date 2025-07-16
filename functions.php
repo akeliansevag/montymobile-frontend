87b50ba651c3098872ffbeabfb1ddad2888e16bb
akeliansevag <akelian.sevag@gmail.com>
2025-07-16 09:43:21 +0300

    .

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '10.1.9');
+	define('_S_VERSION', '11.0.0');
 }
 function remove_jquery_migrate($scripts)
 {
